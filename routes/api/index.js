f8f69958e41c4f1b10ac2c3af6e2a6a87c0725f3
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-11-16 08:53:26 -0700

    error

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
+
+router.use('/comments', commentRoutes)
 //add presfix of /pizzas to routes ceate in pizza-routes.js
 router.use('/pizzas', pizzaRoutes);
 
